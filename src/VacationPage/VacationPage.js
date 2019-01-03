aaf897a18457c2d92c85be6d63349c9c43b9dcd2
Colton <coltonmoler@gmail.com>
2019-01-02 19:07:38 -0500

    more css

--- src/VacationPage/VacationPage.js
+++ src/VacationPage/VacationPage.js
@@ -59,9 +59,9 @@ export class VacationPage extends Component {
                 }];
 
         const slideImages = [
+            'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg',
             'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg',
-            'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg',
-            'https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg'
+            'https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg'
         ];
 
         const properties = {
@@ -147,20 +147,23 @@ export class VacationPage extends Component {
                 <div className="house-info">
                     <h1>House Info</h1>
                     <Slide {...properties}>
-                        <div className="each-slide">
-                            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
-                                <span>Slide 1</span>
+                        <div className="each-fade">
+                            <div className="image-container">
+                                <img src={slideImages[0]} />
                             </div>
+                            <h2>First Slide</h2>
                         </div>
-                        <div className="each-slide">
-                            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
-                                <span>Slide 2</span>
+                        <div className="each-fade">
+                            <div className="image-container">
+                                <img src={slideImages[1]} />
                             </div>
+                            <h2>Second Slide</h2>
                         </div>
-                        <div className="each-slide">
-                            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-                                <span>Slide 3</span>
+                        <div className="each-fade">
+                            <div className="image-container">
+                                <img src={slideImages[2]} />
                             </div>
+                            <h2>Third Slide</h2>
                         </div>
                     </Slide>
                 </div>
